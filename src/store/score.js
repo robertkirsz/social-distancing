786e1c7e2853c0b60c39975114234be8fea01761
Robert Kirsz <robert.kirsz@gmail.com>
2020-05-01 13:00:06 +0200

    Add points

--- src/store/score.js
+++ src/store/score.js
@@ -1,5 +1,28 @@
 import { writable } from 'svelte/store'
 
-const score = writable(0)
+const { subscribe, set, update } = writable(0)
+
+const score = {
+  subscribe,
+  set,
+  update(value) {
+    update(state => state + value)
+    points.animate(Date.now(), value)
+  }
+}
 
 export default score
+
+const { subscribe: pointsSubscribe, update: pointsUpdate } = writable([])
+
+export const points = {
+  subscribe: pointsSubscribe,
+  update: pointsUpdate,
+  animate(id, value) {
+    points.update(state => [...state, { id, value }])
+
+    setTimeout(() => {
+      points.update(state => state.filter(item => item.id !== id))
+    }, 2000)
+  }
+}
