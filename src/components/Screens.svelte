22342fcd9e8f6b78711835cc20212bd98299d410
Robert Kirsz <robert.kirsz@gmail.com>
2020-05-05 19:01:36 +0200

    Animated loading screen

--- src/components/Screens.svelte
+++ src/components/Screens.svelte
@@ -7,7 +7,15 @@
 
 {#if isLoading}
   <div transition:fade class="frost">
-    <h1>Loading...</h1>
+    <h2 class="loading">
+      <span>L</span>
+      <span>o</span>
+      <span>a</span>
+      <span>d</span>
+      <span>i</span>
+      <span>n</span>
+      <span>g</span>
+    </h2>
   </div>
 {/if}
 
@@ -23,3 +31,54 @@
     {/if}
   </div>
 {/if}
+
+<style>
+  .loading {
+    display: flex;
+  }
+
+  .loading span {
+    animation-name: animation;
+    animation-duration: 0.7s;
+    animation-iteration-count: infinite;
+    animation-direction: alternate;
+    animation-fill-mode: both;
+  }
+
+  .loading span:nth-child(1) {
+    animation-delay: 0.1s;
+  }
+
+  .loading span:nth-child(2) {
+    animation-delay: 0.2s;
+  }
+
+  .loading span:nth-child(3) {
+    animation-delay: 0.3s;
+  }
+
+  .loading span:nth-child(4) {
+    animation-delay: 0.4s;
+  }
+
+  .loading span:nth-child(5) {
+    animation-delay: 0.5s;
+  }
+
+  .loading span:nth-child(6) {
+    animation-delay: 0.6s;
+  }
+
+  .loading span:nth-child(7) {
+    animation-delay: 0.7s;
+  }
+
+  @keyframes animation {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+</style>
