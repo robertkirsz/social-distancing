1661ba775639f49d8f5efe2739fb419bd0033e7e
Robert Kirsz <robert.kirsz@gmail.com>
2020-06-03 22:49:33 +0200

    Game over screen improvements

--- src/screens/GameOverScreen.svelte
+++ src/screens/GameOverScreen.svelte
@@ -4,21 +4,24 @@
 </script>
 
 <div in:fade={{ delay: 1000 }} out:fade={{ duration: 200 }} class="screen dots columnTop itemsCenter justifyCenter">
-  <div class="column itemsCenter">
-    <h1 class="center fluid nice" data-text="Game Over">Game Over</h1>
+  <div class="column itemsCenter" style="height: 100%; width: 100%;">
+    <h1 class="center fluid nice marginTopAuto" data-text="Game over">Game over</h1>
 
     <h3 class="rowLeft2 fluid marginTop3" style="white-space: nowrap;">
       <span class="nice" data-text={$score}>{$score}</span>
       <span>points</span>
     </h3>
 
-    <button class="primary small marginTop3" on:click={() => screens.open('RANKING')} data-text="Ranking">
-      Ranking
+    <button class="secondary small marginTop3" on:click={() => screens.open('RANKING')} data-text="See ranking">
+      See ranking
     </button>
 
-    <button class="primary marginTop5" on:click={() => gameIsOver.set(false)} data-text="Play again">Play again</button>
-    <button class="secondary small marginTop5" on:click={() => screens.open('AUTHOR')} data-text="About author">
-      About author
+    <button class="primary marginTopAuto" on:click={() => gameIsOver.set(false)} data-text="Play again">
+      Play again
+    </button>
+
+    <button class="secondary small marginTop2" on:click={() => screens.open('AUTHOR')} data-text="About the author">
+      About the author
     </button>
   </div>
 </div>
