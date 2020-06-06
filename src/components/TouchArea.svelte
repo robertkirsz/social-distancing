728bd213b3ec57da806446bebdbc8e5bebf24c18
Robert Kirsz <robert.kirsz@gmail.com>
2020-06-06 17:13:39 +0200

    Fix mobile controls

--- src/components/TouchArea.svelte
+++ src/components/TouchArea.svelte
@@ -1,24 +1,29 @@
 <script>
-  import { hand } from 'store'
+  import { hand, isExhausted, gameIsOver } from 'store'
 
   const SIZE = 320
   const UNIT = SIZE / 3
 
   function handleTouch(event) {
+    if ($isExhausted || $gameIsOver) {
+      hand.reset()
+      return
+    }
+
     var rect = event.target.getBoundingClientRect()
     var x = Math.floor(event.touches[0].clientX - rect.left)
     var y = Math.floor(event.touches[0].clientY - rect.top)
 
     // prettier-ignore
     const direction =
-      x > 0      && x < UNIT   && y > 0      && y < UNIT   ? 'UP-LEFT' :
-      x > UNIT   && x < UNIT*2 && y > 0      && y < UNIT   ? 'UP' :
-      x > UNIT*2 && x < UNIT*3 && y > 0      && y < UNIT   ? 'UP-RIGHT' :
-      x > 0      && x < UNIT   && y > UNIT   && y < UNIT*2 ? 'LEFT' :
-      x > UNIT*2 && x < UNIT*3 && y > UNIT   && y < UNIT*2 ? 'RIGHT' :
-      x > 0      && x < UNIT   && y > UNIT*2 && y < UNIT*3 ? 'DOWN-LEFT' :
-      x > UNIT   && x < UNIT*2 && y > UNIT*2 && y < UNIT*3 ? 'DOWN' :
-      x > UNIT*2 && x < UNIT*3 && y > UNIT*2 && y < UNIT*3 ? 'DOWN-RIGHT' :
+      x > 0      && x < UNIT   && y > 0      && y < UNIT   ? 'up-left' :
+      x > UNIT   && x < UNIT*2 && y > 0      && y < UNIT   ? 'up' :
+      x > UNIT*2 && x < UNIT*3 && y > 0      && y < UNIT   ? 'up-right' :
+      x > 0      && x < UNIT   && y > UNIT   && y < UNIT*2 ? 'left' :
+      x > UNIT*2 && x < UNIT*3 && y > UNIT   && y < UNIT*2 ? 'right' :
+      x > 0      && x < UNIT   && y > UNIT*2 && y < UNIT*3 ? 'down-left' :
+      x > UNIT   && x < UNIT*2 && y > UNIT*2 && y < UNIT*3 ? 'down' :
+      x > UNIT*2 && x < UNIT*3 && y > UNIT*2 && y < UNIT*3 ? 'down-right' :
       null
 
     const alreadyUp = $hand.direction === direction
@@ -39,5 +44,6 @@
     position: absolute;
     width: 320px;
     height: 320px;
+    border-radius: 50%;
   }
 </style>
