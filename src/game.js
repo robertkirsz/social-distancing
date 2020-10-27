4beb0a81d9011b4a1949e196be48ddb20df94468
Robert Kirsz <robert.kirsz@gmail.com>
2020-10-27 08:40:40 +0100

    Add type generator

--- src/game.js
+++ src/game.js
@@ -1,6 +1,9 @@
 import { projectiles, gameIsRunning, gameIsWon, gameIsOver } from 'store'
 import { coordinates, randomItem, randomNumber } from 'stuff'
 
+const debug = false
+const log = (...args) => debug && console.log(...args)
+
 let game
 
 gameIsRunning.subscribe(isRunning => {
@@ -9,6 +12,20 @@ gameIsRunning.subscribe(isRunning => {
     game.start()
   } else game && game.stop()
 })
+
+function TypeGenerator(type, { amount, chance }, defaultType = 'Stranger') {
+  this.amount = amount
+  this.chance = chance
+
+  return originalType => {
+    if (this.chance > 0 && this.amount > 0 && originalType === defaultType && Math.random() < this.chance) {
+      this.amount--
+      return type
+    }
+
+    return originalType
+  }
+}
 
 function randomize(
   amount,
@@ -27,22 +44,19 @@ function randomize(
     shieldChance = 0.0
   }
 ) {
-  console.log('randomize')
-  let _numberOfFriends = numberOfFriends
+  log('randomize')
+  const friendMaker = new TypeGenerator('Friend', { amount: numberOfFriends, chance: friendChance })
+  const shieldMaker = new TypeGenerator('Shield', { amount: numberOfShields, chance: shieldChance })
+  const lifeMaker = new TypeGenerator('Life', { amount: numberOfLives, chance: lifeChance })
 
   return [...Array(amount)].reduce((acc, _, index) => {
     const previousElement = acc[index - 1] || {}
     const delay = index === 0 ? firstDelay : otherDelays
 
-    // TODO: Make this a function
     let _type = type
-    const rnd = Math.random()
-    console.log({ friends: _numberOfFriends, rnd })
-    if (_numberOfFriends > 0 && rnd < friendChance) {
-      console.log('FRIEND!')
-      _type = 'Friend'
-      _numberOfFriends--
-    }
+    _type = friendMaker(_type)
+    _type = shieldMaker(_type)
+    _type = lifeMaker(_type)
 
     let direction = randomItem(directions)
     if (nonRepeatingDirections) {
@@ -51,46 +65,51 @@ function randomize(
       }
     }
 
-    return [
-      ...acc,
-      {
-        type: _type,
-        delay: typeof delay === 'function' ? delay() : delay,
-        duration: typeof duration === 'function' ? duration() : duration,
-        direction
-      }
-    ]
+    const item = {
+      type: _type,
+      delay: typeof delay === 'function' ? delay() : delay,
+      duration: typeof duration === 'function' ? duration() : duration,
+      direction
+    }
+
+    log(item)
+
+    return [...acc, item]
   }, [])
 }
 
 function circle({ clockwise = true, firstDelay = 1000, otherDelays = 1000, duration = 1500 }) {
-  console.log('circle')
+  log('circle')
   const directions = ['up', 'up-right', 'right', 'down-right', 'down', 'down-left', 'left', 'up-left', 'up']
 
   return (clockwise ? directions : directions.reverse()).map((direction, index) => {
     const delay = index === 0 ? firstDelay : otherDelays
 
-    return {
+    const item = {
       type: 'Stranger',
       delay: typeof delay === 'function' ? delay() : delay,
       duration: typeof duration === 'function' ? duration() : duration,
       direction
     }
+
+    log(item)
+
+    return item
   })
 }
 
 function Game() {
   const levels = [
-    // { delay: 1000, type: 'Stranger', duration: 4200, direction: 'right' },
-    // { delay: 1000, type: 'Stranger', duration: 4200, direction: 'down' },
-    // { delay: 1000, type: 'Stranger', duration: 4200, direction: 'left' },
-    // { delay: 1000, type: 'Stranger', duration: 4200, direction: 'up' },
-    // { delay: 1000, type: 'Friend', duration: 4200, direction: 'down' },
-    // { delay: 3000, type: 'Stranger', duration: 3200, direction: 'up-right' },
-    // { delay: 900, type: 'Stranger', duration: 3200, direction: 'down-right' },
-    // { delay: 900, type: 'Stranger', duration: 3200, direction: 'down-left' },
-    // { delay: 900, type: 'Stranger', duration: 3200, direction: 'up-left' },
-    // { delay: 900, type: 'Shield', duration: 3200, direction: 'down' },
+    { delay: 1000, type: 'Stranger', duration: 4200, direction: 'right' },
+    { delay: 1000, type: 'Stranger', duration: 4200, direction: 'down' },
+    { delay: 1000, type: 'Stranger', duration: 4200, direction: 'left' },
+    { delay: 1000, type: 'Stranger', duration: 4200, direction: 'up' },
+    { delay: 1000, type: 'Friend', duration: 4200, direction: 'down' },
+    { delay: 3000, type: 'Stranger', duration: 3200, direction: 'up-right' },
+    { delay: 900, type: 'Stranger', duration: 3200, direction: 'down-right' },
+    { delay: 900, type: 'Stranger', duration: 3200, direction: 'down-left' },
+    { delay: 900, type: 'Stranger', duration: 3200, direction: 'up-left' },
+    { delay: 900, type: 'Shield', duration: 3200, direction: 'down' },
     ...randomize(6, {
       firstDelay: 3000,
       otherDelays: () => randomNumber(800, 900),
@@ -98,7 +117,7 @@ function Game() {
       numberOfFriends: 2,
       friendChance: 0.3
     }),
-    // ...circle({ clockwise: true, firstDelay: 2000, otherDelays: 600, duration: 1700 }),
+    ...circle({ clockwise: true, firstDelay: 2000, otherDelays: 600, duration: 1700 }),
     ...randomize(10, {
       firstDelay: 1700,
       otherDelays: () => randomNumber(500, 600),
@@ -106,7 +125,7 @@ function Game() {
       numberOfFriends: 2,
       friendChance: 0.3
     }),
-    // ...circle({ clockwise: false, firstDelay: 1500, otherDelays: 500, duration: 1500 }),
+    ...circle({ clockwise: false, firstDelay: 1500, otherDelays: 500, duration: 1500 }),
     ...randomize(10, {
       firstDelay: 1000,
       otherDelays: () => randomNumber(400, 500),
